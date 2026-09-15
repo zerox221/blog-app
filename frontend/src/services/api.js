31a69f0ef7a1d386534254f734994886242abe0b
Balam Chauhan <coding34018@gmail.com>
2026-09-16 03:27:22 +0530

    Update backend and frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Navigate } from "react-router-dom";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 api.interceptors.response.use(
