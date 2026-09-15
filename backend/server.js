2e1cd754947753d92e2a1d8cc2a16cd146f67bc8
Balam Chauhan <coding34018@gmail.com>
2026-09-16 03:34:15 +0530

    Allow local and production frontend origins

--- backend/server.js
+++ backend/server.js
@@ -6,30 +6,39 @@ const userRouter = require("./routes/user");
 const app = express();
 const cors = require("cors");
 const connectCloudinary = require("./config/cloudinary");
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 const dns = require("dns");
-
 
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://blog-app-flax-nine-23.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
 
-dns.setServers([
-  "8.8.8.8",
-  "8.8.4.4"
-]);
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 dns.setDefaultResultOrder("ipv4first");
 
-app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : '/tmp/'
-}));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  }),
+);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
